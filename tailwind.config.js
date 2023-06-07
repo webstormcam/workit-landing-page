05ed6ffa3f8d129f02e88fd01766c1a26afd9fc3
Cameron Chandler <webstormcam@gmail.com>
2023-06-06 22:18:53 -0400

    Placed in both fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}",
   "./*.{html,js}"],
   theme: {
+    fontFamily:{
+manrope:['Manrope','sans-serif'],
+frauces:['Fraunces','sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
